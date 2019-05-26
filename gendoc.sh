a6e912802c1ff40c44b1b5b3c1803bc272741ffb
bendou <sami.dahoux@etu.emse.fr>
2019-05-26 15:54:03 +0200

    fix : https://github.com/samiBendou/meca3/issues/31

--- gendoc.sh
+++ gendoc.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-jsdoc2md ../Vector3.js > Vector3.md
-jsdoc2md ../Matrix3.js > Matrix3.md
-jsdoc2md ../PointPair.js > PointPair.md
-jsdoc2md ../Trajectory.js > Trajectory.md
-jsdoc2md ../BufferTrajectory.js > BufferTrajectory.md
-jsdoc2md ../Solver.js > Solver.md
+jsdoc2md Vector3.js > meca3.wiki/Vector3.md
+jsdoc2md Matrix3.js > meca3.wiki/Matrix3.md
+jsdoc2md PointPair.js > meca3.wiki/PointPair.md
+jsdoc2md Trajectory.js > meca3.wiki/Trajectory.md
+jsdoc2md BufferTrajectory.js > meca3.wiki/BufferTrajectory.md
+jsdoc2md Solver.js > meca3.wiki/Solver.md
