05e409e12a44ce6cc5c0a65cdf3eac7480e32d98
bendou <sami.dahoux@etu.emse.fr>
2019-05-27 15:12:22 +0200

    code : generic generation for all files (https://github.com/samiBendou/meca3/issues/14)

--- gendoc.sh
+++ gendoc.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
-jsdoc2md Vector3.js > meca3.wiki/Vector3.md
-jsdoc2md Matrix3.js > meca3.wiki/Matrix3.md
-jsdoc2md PointPair.js > meca3.wiki/PointPair.md
-jsdoc2md Trajectory.js > meca3.wiki/Trajectory.md
-jsdoc2md BufferTrajectory.js > meca3.wiki/BufferTrajectory.md
-jsdoc2md Solver.js > meca3.wiki/Solver.md
+
+GITHUB_REPO="meca3"
+
+echo "Generating documentation from sources"
+
+for file in *.js
+do
+    filename=$(basename -- "$file")
+    filename="${filename%.*}"
+    jsdoc2md ${file}> ${GITHUB_REPO}.wiki/${filename}.md
+done
+
+rm ${GITHUB_REPO}.wiki/main.md
+
+echo "Successfully generated documentation"
