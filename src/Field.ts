f53a71d849ead2deaac428f31327adb2046477f8
bendou <sami.dahoux@etu.emse.fr>
2019-07-18 00:50:58 +0200

    code : rewriting Field.js in typescript

---
+++ src/Field.ts
@@ -0,0 +1,86 @@
+/**
+ * @brief trajectory of a mobile
+ * @details `Field` class represent a _system_ of material points attracted in a _field_.
+ *
+ * Set `withMass` to `true property to simulate 2nd Newton law dynamic when constructing the f.
+ *
+ * - Move all the points with **dependent dynamic** by giving a unique solver
+ *
+ * - Represent **all the points in the same frame**
+ *
+ */
+import Point from "./Point";
+import Solver, {methods} from "./Solver";
+import Vector3 from "./Vector3";
+
+export default class Field {
+
+    /** points composing the system **/
+    points: Point[];
+
+    /** point used as frame of other points **/
+    frame: Point;
+
+    /** Construct a f by giving an array of points a solver and a frame **/
+    constructor(points: Point[], solver = new Solver(), frame = points[0], withMass = false) {
+        this.points = points;
+        this.frame = frame;
+        this._setSolvers(solver, withMass);
+    }
+
+    /** solver used to update all the points **/
+    get solver() {
+        return this.points[0].solver;
+    }
+
+    set solver(newSolver) {
+        this._setSolvers(newSolver, false);
+    }
+
+    /** barycenter of the points **/
+    get barycenter() {
+        let mass = this.points.reduce((acc, point) => acc + point.mass, 0);
+        return this.points.reduce((acc, point) => acc.add(point.position.mul(point.mass)), Vector3.zeros).div(mass);
+    }
+
+    set barycenter(newCenter) {
+        let center = this.barycenter;
+        this.points.forEach((point) => {
+            point.position = point.position.sub(center).add(newCenter)
+        });
+    }
+
+    private _setSolvers(solver: Solver, withMass: boolean) {
+        this.points.forEach((point) => {
+            let f = solver.f;
+            point.solver = withMass ? new Solver((u) => f(u).div(point.mass), solver.dt0, solver.method) : solver;
+        });
+    }
+
+    /**
+     * @brief updates the position of all the points
+     * @details Solves a step of the ODE of the solver and update position.
+     * @param dt time step for this iteration
+     * @param origin origin to set for the solution
+     * @param method method to use for this step
+     * @returns reference to this
+     */
+    update(dt?: number, origin?: Vector3, method?: methods) {
+        this.points = this.points.map((point) => point.copy().update(dt, method, origin));
+        return this;
+    }
+
+    /**
+     * @brief changes the frame of all the points
+     * @details The whole trajectory of each point is changed.
+     * @param p point to set as frame of each point
+     * @return reference to this
+     */
+    reframe(p: Point) {
+        p = p || Point.zeros();
+        this.points.forEach((q) => {
+            q.reframe(p)
+        });
+        return this;
+    }
+}
---
+++ test/Field.test.ts
@@ -0,0 +1,70 @@
+import {check} from "./common";
+import Vector3 from "../src/Vector3";
+import Point from "../src/Point";
+import Field from "../src/Field";
+import Solver from "../src/Solver";
+
+describe("Point Tests", function () {
+
+    let points: Point[], opoints: Point[];
+    let field: Field, ofield: Field;
+
+    function setUp() {
+        points = [
+            Point.zeros(2, Vector3.zeros, 10),
+            Point.zeros(2, Vector3.zeros, 20),
+            Point.zeros(2, Vector3.zeros, 30)
+        ];
+        opoints = [
+            Point.zeros(2, Vector3.zeros, 10),
+            Point.zeros(2, Vector3.zeros, 20),
+            Point.zeros(2, Vector3.zeros, 30)
+        ];
+
+        let makeField = function (points: Point[]) {
+            return function (u: Vector3) {
+                return points.reduce(function (acc, point) {
+                    return acc.add(point.trajectory.last.position.subc(u).sub(Vector3.ones).div(points.length));
+                }, Vector3.zeros);
+            }
+        };
+        field = new Field(points); // zero f
+        ofield = new Field(opoints, new Solver(makeField(opoints))); // dependent harmonic oscillator
+    }
+
+    it("Rebase", function () {
+        setUp();
+        field.reframe(points[1]);
+        field.points.forEach((p) => {
+            check.arrayEqual(points[1].trajectory.origin, p.trajectory.origin);
+        });
+    });
+
+    it("Update", function () {
+        setUp();
+        field.update();
+        field.points.forEach((point) => {
+            check.equal(point.position, Vector3.zeros);
+        });
+
+        ofield.update();
+        ofield.points.forEach((point) => {
+            check.equal(point.position, Vector3.ones.opp());
+        });
+    });
+
+    it("Barycenter", function () {
+        setUp();
+        check.equal(field.barycenter, Vector3.zeros);
+
+        ofield.update();
+        check.equal(ofield.barycenter, Vector3.ones.opp());
+
+        ofield.barycenter = Vector3.zeros;
+        check.equal(ofield.barycenter, Vector3.zeros);
+
+        ofield.points.forEach((point) => {
+            check.equal(point.position, Vector3.zeros);
+        });
+    })
+});
