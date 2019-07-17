26a604386a5ed2aaa9b7288f8b20dceb85622605
bendou <sami.dahoux@etu.emse.fr>
2019-07-17 22:01:16 +0200

    code : rewriting Trajectory.js in typescript

--- test/Trajectory.test.ts
+++ test/Trajectory.test.ts
@@ -1,20 +1,21 @@
-const assert = require("./common.js");
+import {assert} from "chai";
+import {assets} from "./common";
+import Vector3 from "../src/Vector3";
+import Pair3 from "../src/Pair3";
+import Trajectory from "../src/Trajectory";
+
 
 describe("Trajectory Tests", function () {
-    const Vector3 = require("../src/Vector3.js");
-    const PointPair = require("../src/PointPair.js");
-    const Trajectory = require("../src/Trajectory.js");
 
-
-    let dt;
-    let om0, om1, om2, om3, om4;
-    let gamma0, gamma1, gamma2;
+    let dt: number;
+    let om0: Pair3, om1: Pair3, om2: Pair3, om3: Pair3, om4: Pair3;
+    let gamma0: Trajectory, gamma1: Trajectory, gamma2: Trajectory;
 
     function setUp() {
         dt = 0.1;
-        om0 = PointPair.vect(Vector3.ex);
-        om1 = PointPair.vect(Vector3.ey);
-        om2 = PointPair.vect(Vector3.ex.opp());
+        om0 = Pair3.vect(Vector3.ex);
+        om1 = Pair3.vect(Vector3.ey);
+        om2 = Pair3.vect(Vector3.ex.opp());
         om3 = om0.copy().translate(Vector3.ex);
         om4 = om0.copy().translate(Vector3.ey);
 
@@ -31,15 +32,15 @@ describe("Trajectory Tests", function () {
 
     it("Positions", function () {
         setUp();
-        assert.meca3.equal(gamma0, Trajectory.discrete([om0.relative, om1.relative, om2.relative], dt));
-        assert.meca3.equal(gamma1, Trajectory.discrete([Vector3.ex, Vector3.ex, Vector3.ex], dt));
+        assets.equal(gamma0, Trajectory.discrete([om0.relative, om1.relative, om2.relative], dt));
+        assets.equal(gamma1, Trajectory.discrete([Vector3.ex, Vector3.ex, Vector3.ex], dt));
     });
 
     it("Add", function () {
         setUp();
-        let pp = new PointPair(om1.origin.copy(), om1.vector.oppc());
-        assert.meca3.equal(gamma0.add(pp), new Trajectory([om0, om1, om2, pp], dt));
-        assert.meca3.equal(gamma2.add(om1), new Trajectory([om0, om1], 1));
+        let pp = new Pair3(om1.origin.copy(), om1.position.oppc());
+        assets.equal(gamma0.add(pp), new Trajectory([om0, om1, om2, pp], dt));
+        assets.equal(gamma2.add(om1), new Trajectory([om0, om1], 1));
     });
 
     it("Clear", function () {
@@ -52,33 +53,33 @@ describe("Trajectory Tests", function () {
     it("Origins", function () {
         setUp();
         let originToSet = [Vector3.zeros, Vector3.zeros, Vector3.zeros];
-        assert.meca3.arrayEqual(gamma1.origin, [om0.origin, om3.origin, om4.origin]);
+        assets.arrayEqual(gamma1.origin, [om0.origin, om3.origin, om4.origin]);
 
         gamma1.origin = originToSet;
-        assert.meca3.arrayEqual(gamma1.origin, originToSet);
+        assets.arrayEqual(gamma1.origin, originToSet);
     });
 
     it("First/Last/Nexto", function () {
         setUp();
-        assert.meca3.equal(gamma0.first, om0);
-        assert.meca3.equal(gamma0.nexto, om1);
-        assert.meca3.equal(gamma0.last, om2);
+        assets.equal(gamma0.first, om0);
+        assets.equal(gamma0.nexto, om1);
+        assets.equal(gamma0.last, om2);
 
         gamma0.first = om1;
         gamma0.nexto = om4;
         gamma0.last = om0;
 
-        assert.meca3.equal(gamma0.first, om1);
-        assert.meca3.equal(gamma0.nexto, om4);
-        assert.meca3.equal(gamma0.last, om0);
+        assets.equal(gamma0.first, om1);
+        assets.equal(gamma0.nexto, om4);
+        assets.equal(gamma0.last, om0);
     });
 
     it("At/Get", function () {
         setUp();
-        assert.meca3.equal(gamma0.get(0), om0);
-        assert.meca3.equal(gamma0.at(0), om0);
-        assert.meca3.equal(gamma0.get(1), om1);
-        assert.meca3.equal(gamma0.at(0.5).vector, Vector3.ey);
+        assets.equal(gamma0.get(0), om0);
+        assets.equal(gamma0.at(0), om0);
+        assets.equal(gamma0.get(1), om1);
+        assets.equal(gamma0.at(0.5).position, Vector3.ey);
     });
 
     it("Time", function () {
@@ -93,41 +94,41 @@ describe("Trajectory Tests", function () {
 
     it("Origin", function () {
         setUp();
-        assert.meca3.equal(
+        assets.equal(
             gamma0,
-            Trajectory.discrete([om0.vector, om1.vector, om2.vector], 0.1, Vector3.zeros));
+            Trajectory.discrete([om0.position, om1.position, om2.position], 0.1, Vector3.zeros));
     });
 
     it("Generate linear", function () {
         let gamma2 = Trajectory.linear(3);
         let expect2 = Trajectory.discrete([Vector3.zeros, Vector3.ex, Vector3.ex.mul(2)]);
-        assert.meca3.equal(gamma2, expect2);
+        assets.equal(gamma2, expect2);
     });
 
     it("Generate circle", function () {
-        let gamma2 = Trajectory.circular(0.25);
+        let gamma2 = Trajectory.circular(1, Vector3.ez, Vector3.zeros, 0.25);
         let expect2 = Trajectory.discrete([Vector3.ex, Vector3.ey, Vector3.ex.opp(), Vector3.ey.opp()]);
-        assert.meca3.equal(gamma2, expect2);
+        assets.equal(gamma2, expect2);
 
-        let gamma3 = Trajectory.circular(0.25, 1, Vector3.ex);
+        let gamma3 = Trajectory.circular(1, Vector3.ex, Vector3.zeros, 0.25);
         let expect3 = Trajectory.discrete([Vector3.ey, Vector3.ez, Vector3.ey.opp(), Vector3.ez.opp()]);
-        assert.meca3.equal(gamma3, expect3);
+        assets.equal(gamma3, expect3);
 
-        let gamma4 = Trajectory.circular(0.25, 1, Vector3.ey);
+        let gamma4 = Trajectory.circular(1, Vector3.ey, Vector3.zeros, 0.25);
         let expect4 = Trajectory.discrete([Vector3.ex.opp(), Vector3.ez, Vector3.ex, Vector3.ez.opp()]);
-        assert.meca3.equal(gamma4, expect4);
+        assets.equal(gamma4, expect4);
     });
 
     it("Generate ellipse", function () {
         let tol = 1.3 * Number.EPSILON;
-        let gamma2 = Trajectory.elliptic(0.25, 1.5);
+        let gamma2 = Trajectory.elliptic(1.5, 1, Vector3.ez, Vector3.zeros, 0.25);
         let expect2 = [Vector3.ex.mul(1.5), Vector3.ey, Vector3.ex.mul(-1.5), Vector3.ey.opp()];
-        assert.meca3.arrayEqual(gamma2.relative, expect2, tol);
+        assets.arrayEqual(gamma2.relative, expect2, tol);
     });
 
     it("Generate hyperbola", function () {
         let tol = 1e-10;
-        let gamma2 = Trajectory.hyperbolic(0.25);
+        let gamma2 = Trajectory.hyperbolic(1, 1, Vector3.ez, Vector3.zeros, 0.25);
         gamma2.relative.forEach((u, index) => {
             assert.approximately(u.x ** 2 - u.y ** 2, 1, tol, `\n${u}\n index=${index}`);
         });
