29698cccbc952202bfb195ee17c3e0ee6e4f8a1d
bendou <sami.dahoux@etu.emse.fr>
2019-07-18 20:14:16 +0200

    code : adding barrel for both npm and typescript use

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+// ENTRY POINT FOF NPM PACKAGE
+
+export * from "./Vector3";
+export * from "./Matrix3";
+export * from "./Pair3";
+export * from "./Trajectory";
+export * from "./BufferTrajectory";
+export * from "./Solver";
+export * from "./Point";
+export * from "./Field";
