cbe6c92ddfbbe7ee7979ce0ecf18cbfa55a5a8e3
Sami Dahoux <sami.dahoux@aletiq.com>
2022-04-04 17:48:53 +0200

    Points as state space vectors

--- src/Field.ts
+++ src/Field.ts
@@ -1,43 +1,39 @@
-import { Solver, Vector3, Vector6, VectorField } from "../../space3/src";
+import { Solver, Vector3, VectorField } from "../../space3/src";
 import Point from "./Point";
 
 type AccelerationField = (
   acceleration: Vector3,
-  position: Vector3,
-  speed: Vector3,
+  point: Point,
   time?: number
 ) => Vector3;
 
-export default class Field<T extends Point> {
-  points: T[];
-  private field: VectorField<Vector6>;
-  private positionBuffer: Vector3;
-  private speedBuffer: Vector3;
+export default class Field {
+  points: Point[];
+  private field: VectorField<Point>;
+  private pointBuffer: Point;
   private accelerationBuffer: Vector3;
-  private solvers: Solver<Vector6>[];
-  constructor(points: T[], field: AccelerationField, dt?: number) {
+  private solvers: Solver<Point>[];
+  constructor(points: Point[], field: AccelerationField, dt?: number) {
     this.points = points;
 
+    this.pointBuffer = new Point(0);
     this.accelerationBuffer = Vector3.zeros;
-    this.positionBuffer = Vector3.zeros;
-    this.speedBuffer = Vector3.zeros;
-    this.field = (u?: Vector6, t?: number) => {
-      this.positionBuffer.xyz = u.upper;
-      this.speedBuffer.xyz = u.lower;
+
+    this.field = (p?: Point, t?: number) => {
       this.accelerationBuffer.xyz = [0, 0, 0];
+      this.pointBuffer.copy(p);
       this.accelerationBuffer = field(
         this.accelerationBuffer,
-        this.positionBuffer,
-        this.speedBuffer,
+        this.pointBuffer,
         t
       );
-      u.upper = this.speedBuffer.xyz;
-      u.lower = this.accelerationBuffer.xyz;
-      return u;
+      p.position = this.pointBuffer.speed;
+      p.speed = this.accelerationBuffer;
+      return p;
     };
 
     this.solvers = this.points.map(
-      (point) => new Solver(this.field, dt, point.state)
+      (point) => new Solver(this.field, dt, point)
     );
   }
 
@@ -49,10 +45,10 @@ export default class Field<T extends Point> {
    */
   update(dt?: number): this {
     const states = this.points.map((point, idx) =>
-      this.solvers[idx].step(point.state, dt)
+      this.solvers[idx].step(point, dt)
     );
     this.points.forEach((point, idx) => {
-      point.state = states[idx];
+      point.copy(states[idx]);
     });
 
     return this;
--- src/Point.ts
+++ src/Point.ts
@@ -1,4 +1,6 @@
 import { Vector3, Vector6 } from "../../space3/src";
+import List from "../../space3/src/common/List";
+import Vectorial from "../../space3/src/common/Vectorial";
 
 /**
  * @brief material point
@@ -11,7 +13,7 @@ import { Vector3, Vector6 } from "../../space3/src";
  * - **Manipulate speed and position** of the point
  *
  */
-export default class Point {
+export default class Point implements Vectorial, List {
   /** mass of the point **/
   mass: number;
 
@@ -27,6 +29,82 @@ export default class Point {
     this._position = position ? position.clone() : Vector3.zeros;
     this._speed = speed ? speed.clone() : Vector3.zeros;
     this._state = new Vector6(...this.position.xyz, ...this.speed.xyz);
+  }
+  array(): number[] {
+    return this._state.array();
+  }
+
+  string(): string {
+    return this._state.string();
+  }
+
+  assign(...coordinates: number[]): this {
+    this._position.assign(coordinates[0], coordinates[1], coordinates[2]);
+    this._speed.assign(coordinates[3], coordinates[4], coordinates[5]);
+    this._state.assign(
+      coordinates[0],
+      coordinates[1],
+      coordinates[2],
+      coordinates[3],
+      coordinates[4],
+      coordinates[5]
+    );
+    return this;
+  }
+
+  copy(point: this): this {
+    this._position.copy(point._position);
+    this._speed.copy(point._speed);
+    this._state.copy(point._state);
+    return this;
+  }
+
+  clone(): this {
+    return new Point(this.mass, this.position, this.speed) as this;
+  }
+
+  dim: Readonly<number> = 6;
+
+  add(point: Point): this {
+    this._position.add(point._position);
+    this._speed.add(point._speed);
+    this._state.add(point._state);
+    return this;
+  }
+
+  sub(point: this): this {
+    this._position.sub(point._position);
+    this._speed.sub(point._speed);
+    this._state.sub(point._state);
+    return this;
+  }
+
+  neg(): this {
+    this._position.neg();
+    this._speed.neg();
+    this._state.neg();
+    return this;
+  }
+
+  mul(s: number): this {
+    this._position.mul(s);
+    this._speed.mul(s);
+    this._state.mul(s);
+    return this;
+  }
+
+  div(s: number): this {
+    this._position.div(s);
+    this._speed.div(s);
+    this._state.div(s);
+    return this;
+  }
+
+  comb(s: number, point: this): this {
+    this._position.comb(s, point._position);
+    this._speed.comb(s, point._speed);
+    this._state.comb(s, point._state);
+    return this;
   }
 
   get position() {
